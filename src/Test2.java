389ad035e425f6080b0d2714a3eab44f68fa1340
alumno <alumno@host01.example.com>
2020-09-08 16:59:44 -0500

    Commiting Test2 to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+	
+	public static void main(String args[]) {
+		System.out.println("WWWWW::Test2::main::start");
+		
+		System.out.println("WWWWW::Test2::main::end");
+	}
+
+}
