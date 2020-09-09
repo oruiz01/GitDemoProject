a6c50ed199aea2f039c732d0b3dee8725e11b452
alumno <alumno@host01.example.com>
2020-09-08 22:58:47 -0500

    Anexamos modificaciones S

--- src/Test2.java
+++ src/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	public static void main(String args[]) {
 		System.out.println("WWWWW::Test2::main::start");
 		
+		System.out.println("WWWWW::Modificacion adicional");
 		System.out.println("WWWWW::Test2::main::end");
 	}
 
